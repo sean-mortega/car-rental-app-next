0ad080171c9e074bc465199e31641025d8668e94
Sean <mortega.sean@gmail.com>
2023-06-15 11:55:01 +0800

    added api key

--- utils/index.ts
+++ utils/index.ts
@@ -46,7 +46,7 @@ export async function fetchCars(filters: FilterProps) {
 
   // Set the required headers for the API request
   const headers: HeadersInit = {
-    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+    "X-RapidAPI-Key": 'f5905f5e51msh398d7117ef20a51p10eac5jsn462febe63576' || "",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
